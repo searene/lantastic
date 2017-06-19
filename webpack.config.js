e289d691d36ace10c861b687a1e540d478ef5460
searene <mycityofsky@gmail.com>
2017-06-19 22:52:19 +0800

    import font works

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: "./src/main.ts",
+    entry: ["./src/main.ts", "./src/sass/style.scss"],
     output: {
         filename: "main.js",
         path: __dirname + "/lib"
     },
     target: 'electron-main',
+    node: {
+        __dirname: false,
+        __filename: false
+    },
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
@@ -24,7 +29,17 @@ module.exports = {
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'file-loader?publicPath=../&outputPath=font/' }
         ]
     },
 
@@ -46,7 +61,8 @@ module.exports = {
         new CopyWebpackPlugin([{
             from: path.join(__dirname, 'src'),
             to: path.join(__dirname, 'lib'),
-            ignore: ['*.tsx', '*.ts']
-        }])
+            ignore: ['*.tsx', '*.ts', '*.scss', '*.ttf']
+        }]),
+        new ExtractTextPlugin('css/style.css'),
     ]
 };
