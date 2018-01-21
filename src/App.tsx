ff77c5a8db5d6675e1a2b0c8fd29b87f7fcb4201
searene <mycityofsky@gmail.com>
2018-01-21 11:01:13 +0800

    Revert "remove semantic ui and components"
    This reverts commit d73afc679f081c1560913675021c00aec058b6aa.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,69 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Radium from 'radium';
+import * as SplitPane from 'react-split-pane';
+import { Grid, Input, Button, Container } from 'semantic-ui-react';
+import { Dictionary } from './components/Dictionary';
+import { Field } from './components/Field';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+import { Preference } from './components/Preference';
+import Split = require('split.js');
 
-export class App extends React.Component<{}, {}> {
+export interface AppProps {
+  deck: string;
+  type: string;
+}
+
+@Radium
+export class App extends React.Component<AppProps, undefined> {
+  componentDidMount() { 
+    Split(['#navbar', '#field'], {
+      direction: 'horizontal',
+      minSize: [150, 100],
+    });
+  }
   render() {
+    const styles: React.CSSProperties = {
+      typeButton: {
+        width: "40%",
+      }, 
+      buttonGroup: {
+        width: "50%",
+        paddingRight: "5px",
+      },
+      container: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "column",
+      },
+      row1: {
+        height: "100%",
+        display: "flex",
+        width: "100%",
+        flex: "1",
+        overflow: "auto",
+      },
+    };
+    let descriptions: string[] = ['front', 'back'];
     return (
-      <div>App</div>
+      <div style={styles.container}>
+        <div style={styles.row1} id="row1">
+            <Navbar />
+            <Field descriptions={descriptions}/>
+        </div>
+        <div style={styles.row2} id="row2">
+          <Footer deck="Default" type="Basic" />
+        </div>
+      </div>
     );
   }
 }
 
 ReactDOM.render(
-  <App />,
+  // <App deck="Default" type="Basic" />,
+  <Preference />,
   document.getElementById('app')
 );
