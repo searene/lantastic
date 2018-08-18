4696d0bc8792ca44669963f19adcd5913c2420d7
searene <mycityofsky@gmail.com>
2018-08-18 11:34:00 +0800

    update FileSystemPC

--- src/os-specific/FileSystemPC.ts
+++ src/os-specific/FileSystemPC.ts
@@ -1,20 +1,21 @@
 import * as fse from "fs-extra";
-import { IFileSystem } from "dict-parser";
 import { pathExists, ReadResult } from "fs-extra";
+import { IFileSystem } from "dict-parser";
+import { Buffer } from "buffer";
 
 export class FileSystemPC implements IFileSystem {
   public isDir = async (filePath: string): Promise<boolean> => {
     return (await fse.lstat(filePath)).isDirectory();
-  }
+  };
   public createFile = async (filePath: string): Promise<void> => {
     await fse.createFile(filePath);
   };
   public pathExists = async (filePath: string): Promise<boolean> => {
     return await pathExists(filePath);
-  }
+  };
   public open = async (path: string, flags: string | number, mode?: number): Promise<number> => {
     return await fse.open(path, flags, mode);
-  }
+  };
   public close = async (fd: number): Promise<void> => {
     return await fse.close(fd);
   };
@@ -23,14 +24,31 @@ export class FileSystemPC implements IFileSystem {
       throw new Error("fdOrFilePath must be a number on PC");
     }
     return await fse.read(fdOrFilePath, Buffer.alloc(length), 0, length, position);
-  }
+  };
   public readFile = async (filePath: string): Promise<Buffer> => {
     return await fse.readFile(filePath);
   };
   public readdir = async (dir: string): Promise<string[]> => {
     return await fse.readdir(dir);
   };
+  public writeFile = async (filePath: string, contents: string): Promise<void> => {
+    return await fse.writeFile(filePath, contents);
+  };
   public exists = async (filePath: string): Promise<boolean> => {
     return await fse.pathExists(filePath);
-  }
+  };
+  public getSize = async (filePath: string): Promise<number> => {
+    const stat = await fse.lstat(filePath);
+    return stat.size;
+  };
+  public readWithBufferOffset = async (
+    fd: number,
+    buffer: Buffer,
+    offset: number,
+    length: number,
+    position: number
+  ): Promise<ReadResult> => {
+    return await fse.read(fd, buffer, offset, length, position);
+  };
 }
+
